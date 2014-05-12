0c050ed0d094b910568d436209279da465a6a9ac
hotoo <hotoo.cn@gmail.com>
2014-05-12 19:23:09 +0800

    Update for spm@3.0.0, and integrated source for node.

---
+++ src/morerule.js
@@ -0,0 +1,16 @@
+module.exports = {
+  DEVICES: [],
+  OS: [
+    ["meego", /\bmeego\b/]
+  ],
+  BROWSER: [
+    ["googlebot", /\bgooglebot\/([0-9.]+)/],
+    ["msnbot", /\bmsnbot-media \/([0-9.a-z]+)/],
+    ["nuhkbot", /\bnuhk\/([0-9.]+)/],
+    ["yammybot", /\byammybot\b/],
+    ["openbot", /\bopenbot\b/],
+    ["slurpbot", /\bslurp\b/],
+    ["ia_archiver", /\bia_archiver\b/]
+  ],
+  ENGINE: []
+};
